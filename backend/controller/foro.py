5674b67dc824b8af435fb6ae10ec613ca95f4330
Marco F. Correa <casacorrea50@gmail.com>
2022-11-10 16:57:30 -0300

    Crud

--- backend/controller/foro.py
+++ backend/controller/foro.py
@@ -7,17 +7,14 @@ def __init__(self):
         host ='localhost',
         port = '3306',
         user = 'root',
-        password = 'root'
+        password = 'Imago2010',
         db = 'El-anzuelo' 
     )
     except mysql.connector.Error as descrpcionError:
         print ("No se coneccto", descrpcionError)
 
 
-
-   
-
-
+#Ingresar consulta a la seccion FORO - INSERT
 def InsertarPost(self, titulo, post):
     if self.conexion.is_connected():
         try:
@@ -31,6 +28,78 @@ def InsertarPost(self, titulo, post):
        
         except:
             print ("No se pudo conectar a la base de datos")
+
+
+
+
+#Leer consulta a la seccion FORO 
+def buscarPost (self):
+    if self.conexion.is_connected():
+        try:
+            cursor= self.conexion.cursor()
+            sentenciaSQL= "SELECT * from post where titulo like '%DORADO%'" 
+            cursor.execute(sentenciaSQL)
+            resultadoFORO = cursor.fetchall()
+            self.conexion.close()
+            return resultadoFORO
+        
+        except:
+             print ("No se pudo conectar a la base de datos")
+
+
+
+
+
+#Eliminar consulta seccion FORO - DELETE
+def EliminarPost (self, ID):
+    if self.conexion.is_connected():
+        try:
+            cursor= self.conexion.cursor()
+            sentenciaSQL = "DELETE from where post where id=ID"
+            cursor.execute(sentenciaSQL)
+
+            self.conexion.commit()
+            self.conexion.closed()
+
+        except:
+             print ("No se pudo conectar a la base de datos")
+
+
+
+#Ingresar consulta a la seccion FORO - INSERT
+def InsertarPost(self, titulo, post):
+    if self.conexion.is_connected():
+        try:
+            cursor= self.conexion.cursor()
+            sentenciaSQL= "INSERT INTO post VALUES(%s,%s)"
+            data= (titulo,post)
+
+            cursor.execute(sentenciaSQL,data)
+            self.conexion.commit()
+            self.conexion.closed()
+       
+        except:
+            print ("No se pudo conectar a la base de datos")
+
+
+
+
+#Cambiar consulta a la seccion FORO - UPDATE
+def InsertarPost(self, titulo, post):
+    if self.conexion.is_connected():
+        try:
+            cursor= self.conexion.cursor()
+            sentenciaSQL= "UPDATE INTO post VALUES(%s,%s)"
+            data= (titulo,post)
+
+            cursor.execute(sentenciaSQL,data)
+            self.conexion.commit()
+            self.conexion.closed()
+       
+        except:
+            print ("No se pudo conectar a la base de datos")
+
+
 
 
 
