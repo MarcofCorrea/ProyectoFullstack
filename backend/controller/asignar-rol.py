ed04b95dc664f1c427b02b2cd95a0c081b7b685b
mariquenaallosa <allosamariquena@gmail.com>
2022-11-12 19:24:26 -0300

    agregar controladores

---
+++ backend/controller/asignar-rol.py
@@ -0,0 +1,6 @@
+# EL administrador debería ser el único con acceso a esta función ya que es el que administra los roles dentro del sitio
+# busca un usuario
+# asigna un rol
+
+def asignarRol(usuario,rol):
+    pass
