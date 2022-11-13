6a1376941b5bd846302dbe7d86c0b419a5f811a1
mariquenaallosa <allosamariquena@gmail.com>
2022-11-13 18:37:21 -0300

    model

--- backend/model/producto.py
+++ backend/model/producto.py
@@ -1,1 +1,30 @@
-#producto.py#
+class Producto:
+    def __init__(self, nom, des, pre,prodId):
+        self.nombre=nom
+        self.descripcion=des
+        self.precio=pre
+        self.pId=prodId
+    def __str__(self):
+        return f'Nombre producto: {self.nombre}, Descripción: {self.descripcion},Precio: {self.precio},ID producto: {self.pId}.'
+    #Get&Set Nombre
+    def getNombre(self):
+        return self.nombre
+    def setNombre(self, n):
+        self.nombre = n
+    #Get&Set Descripcio
+    def getDescripcion(self):
+        return self.descripcion
+    def setDescripcion(self, n):
+        self.descripcion = n
+    #Get&Set Precio
+    def getPrecio(self):
+        return self.precio
+    def setPrecio(self, n):
+        self.precio = n
+
+    #Get&Set id de producto
+    def getId(self):
+        return self.pId
+    def setId(self, n):
+        self.pId = n
+
