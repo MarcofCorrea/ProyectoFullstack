70f1c2a32f9de45fd9551c1195efde77e02413bf
mariquenaallosa <allosamariquena@gmail.com>
2022-11-13 21:02:06 -0300

    revertir cambiar de commit

--- backend/model/Categoria.py
+++ backend/model/Categoria.py
@@ -1,12 +1,1 @@
-class Categoria:
-    def __init__(self,idCat):
-        self.idCategoria = idCat
-    def __str__(self):
-        return f'Categoria: {self.idCategoria}'
-    #Get&Set Categoria
-    def getId(self):
-        return self.idCategoria
-    def setId(self, idCategoria):
-        self.idCategoria = idCategoria
-
-
+#categoria.py#
